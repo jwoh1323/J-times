7bddf6aaf4e39bdccf8bf372a8618027b60c5a21
Jinwoo Oh <44737873+jwoh1323@user.nonreply.github.com>
2019-03-26 20:22:45 -0500

    adding codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,11 +35,11 @@ d3.csv("/assets/data/data.csv").then (function(statedata) {
   });
   
   var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(statedata, d => d.poverty))
+    .domain([d3.min(statedata, d => d.poverty * 0.8), d3.max(statedata, d => d.poverty * 1.2)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(statedata, d => d.healthcare)])
+    .domain([d3.min(statedata, d => d.healthcare * 0.8), d3.max(statedata, d => d.healthcare * 1.2)])
     .range([height, 0]);
 
   var bottomAxis = d3.axisBottom(xLinearScale);
@@ -56,10 +56,36 @@ d3.csv("/assets/data/data.csv").then (function(statedata) {
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", 10)
-    .attr("fill", "lightblue");
+    .attr("fill", "skyblue");
+
+  chartGroup.selectAll(".text")
+    .data(statedata)
+    .enter()
+    .append("text")
+    .text(d => d.abbr)
+    .attr("x", d => xLinearScale(d.poverty)-6)
+    .attr("y", d => yLinearScale(d.healthcare)+3)
+    .style("font", "8px sans-serif")
+    .style("fill", "white")
+    .attr({"text-anchor" : "middle"})
 
   chartGroup.append("text")
-    .text(d => d.abbr)
-    .attr("x", d => xLinearScale(d.poverty))
-    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 1.5))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Lacks Healthcare (%)")
+    .attr("font-weight", 700)
+
+  
+  var labelsGroup = chartGroup.append("g")
+  .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  labelsGroup.append("text")
+  .attr("x", 0)
+  .attr("y", 20)
+  .classed("active", true)
+  .text("In Poverty (%)");
+
   });
